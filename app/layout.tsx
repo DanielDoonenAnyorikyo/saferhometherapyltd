6f08252d339cb0691e5629c3c17ed9b1059dd821
Daniel Neo-Tech <doonendanielanyorikyo@gmail.com>
2026-03-09 18:47:42 +0100

    Update projectID in voiceflow chat integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
                 var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
                 v.onload = function() {
                   window.voiceflow.chat.load({
-                    verify: { projectID: '69a7eb2e7e59042952d660a2' },
+                    verify: { projectID: '69a7eb2e7e59042952d660a3' },
                     url: 'https://general-runtime.voiceflow.com',
                     versionID: 'production',
                     voice: {
